069061f84e05a95f3c285628ae53eb04087abc37
Zahari Dzhelepov <253925@via.dk>
2018-04-10 10:53:40 +0200

    led 3 blinking

--- HelloWorld/HelloWorld/main.c
+++ HelloWorld/HelloWorld/main.c
@@ -1,10 +1,21 @@
 
 #define F_CPU 10000000L
 #include <avr/io.h>
+#include <util/delay.h>
 
 
 int main(void)
 {
     DDRA = 0xff;            // setting all I/O's in port A as outputs
-    PORTA = ~(1<<PA3);      // turning on led 0 (active low)
+	
+	while (1)
+	{
+		PORTA = ~(1<<PA3);      // turning on led 3 (active low)
+		_delay_ms(500);
+		PORTA = ~(0<<PA3);		// turning off led3
+		_delay_ms(500);
+	}
+	
+	
+	
 }
